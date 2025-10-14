99582ee52134b295c829f8c6ec1495a5b141c852
Rohit Kumar Singh <49697906+rohity294@users.noreply.github.com>
2025-10-14 01:52:26 +0000

    c3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
